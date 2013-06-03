c527fbb220541d744dfe7a23c7c60d4380b453e3
kensuvy <kensuvy@gmail.com>
2013-06-03 15:04:35 +0530

    added two new files

---
+++ Sabdakon.java
@@ -0,0 +1,28 @@
+/*
+	Copyright 2013, Subhankar Sahu
+
+	Author(s): Subhankar Sahu                                 
+	Public Repo: https://github.com/kensuvy/Sabdakon                                                
+
+	This file is part of Sabdakon.
+
+    	Sabdakon is free software: you can redistribute it and/or modify
+   	it under the terms of the GNU General Public License as published by
+    	the Free Software Foundation, either version 3 of the License, or
+    	(at your option) any later version.
+
+    	Sabdakon is distributed in the hope that it will be useful,
+    	but WITHOUT ANY WARRANTY; without even the implied warranty of
+    	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    	GNU General Public License for more details.
+
+    	You should have received a copy of the GNU General Public License
+    	along with Sabdakon.  If not, see <http://www.gnu.org/licenses/>.
+	
+*/
+
+/*	
+ 	Sabdakon.java	
+
+  	This is a the main executable file.
+*/
